006f248f33af77452c8983af14b2bfc7a9ef755d
spaezsuarez <spaezsuarez@gmail.com>
2021-01-07 05:37:03 -0500

    Eliminando codigo innecesario en la clase DAO

--- src/common/Dao.js
+++ src/common/Dao.js
@@ -3,65 +3,35 @@ const resource = require('./proccesData');
 
 class Dao{
 
-    constructor(){
-        this.db = db;
+    sendRequest(request){
+        return new Promise((resolve,reject) => {
+            db.query(request)
+                .then((response) => {
+                    resolve(response);
+                }).catch((error) => {
+                    reject(error);
+                });
+        });
     }
 
     getAll(table){
-        return new Promise((resolve,reject) => {
-            this.db.query(`SELECT * FROM ${table};`)
-                .then((response) => {
-                    resolve(response);
-                }).catch((error) => {
-                    reject(error);
-                });
-        });
+        return this.sendRequest(`SELECT * FROM ${table};`);
     }
 
     get(table,id){
-        return new Promise((resolve,reject) =>{
-            this.db.query(`SELECT * FROM ${table} WHERE ${resource.getIdDB(table)} = ${id};`)
-                .then((response) => {
-                    resolve(response);
-                }).catch((error) => {
-                    reject(error);
-                });
-        });
+        return this.sendRequest(`SELECT * FROM ${table} WHERE ${resource.getIdDB(table)} = ${id};`);
     }
 
     create(table,data){
-        return new Promise((resolve,reject) => {
-            this.db.query(`INSERT INTO ${table} ${resource.getValueText(data)}`)
-                .then((data) => {
-                    resolve(data);
-                }).catch((error) => {
-                    reject(error);
-                });
-        });
+        return this.sendRequest(`INSERT INTO ${table} ${resource.getValueText(data)}`);
     }
 
     delete(table,id){
-        return new Promise((resolve,reject) =>{
-            this.db.query(`DELETE FROM ${table} WHERE ${resource.getIdDB(table)} = ${id};`)
-                .then((response) => {
-                    resolve(response);
-                }).catch((error) => {
-                    reject(error);
-                });
-        });
+       return this.sendRequest(`DELETE FROM ${table} WHERE ${resource.getIdDB(table)} = ${id};`);
     }
 
     update(table,body,id){
-        return new Promise((resolve,reject) => {
-            this.db.query(`UPDATE ${table} SET ${resource.getUpdateText(body)} WHERE ${resource.getIdDB(table)} = ${id};`)
-                .then((response) => {
-                    resolve(response);
-                })
-                .catch((error) => {
-                    reject(error);
-                });
-        });
-
+        return this.sendRequest(`UPDATE ${table} SET ${resource.getUpdateText(body)} WHERE ${resource.getIdDB(table)} = ${id};`);
     }
 
     

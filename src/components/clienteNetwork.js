662d7eee034096ac8a37f344745bad9c1885e449
spaezsuarez <spaezsuarez@gmail.com>
2021-01-08 04:10:18 -0500

    Refactor folder of components

---
+++ src/components/clienteNetwork.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const routerClient = express.Router();
+const Dao = require('../common/Dao');
+const { succes , error } = require('../common/response');
+const { createDataMiddleware } = require('../common/middlewares');
+
+let clientDao = new Dao();
+
+routerClient.get('/',(request,response) => {
+    clientDao.getAll('Cliente')
+        .then((res) => {
+            succes(request,response,res,200);
+        })
+        .catch((e) => {
+            error(request,response,e,500);
+        });
+});
+
+routerClient.get('/ver/:id?',(request,response) => {
+    clientDao.get('Cliente',request.query.id)
+        .then((res) => {
+            succes(request,response,res,200);
+        })
+        .catch((e) => {
+            error(request,response,e,500);
+        });
+});
+
+routerClient.post('/create',createDataMiddleware,(request,response) => {
+    clientDao.create('Cliente',request.values)
+        .then((data) => {
+            succes(request,response,data,201);
+        })
+        .catch((err) => {
+            error(request,response,err,501);
+        });
+})
+
+
+module.exports = routerClient;

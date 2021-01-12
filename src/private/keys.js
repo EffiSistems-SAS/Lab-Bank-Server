dd10e8a5321ab694781e98c9380f991283b15bf1
spaezsuarez <spaezsuarez@gmail.com>
2021-01-12 11:42:15 -0500

    Refactor credenciales de base de datos v0.0

--- src/private/keys.js
+++ src/private/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
-    host     :  process.env.DATABASE_HOST | 'localhost',
-    user     :  process.env.DATABASE_USER | 'spaezsuarez',
+    host     :  process.env.DATABASE_HOST || 'localhost',
+    user     :  process.env.DATABASE_USER || 'spaezsuarez',
     password :  process.env.DATABASE_PASSWORD ||'tintaculta23',
-    database :  process.env.DATABASE ||'atm'
+    database :  process.env.DATABASE || 'atm'
   }
